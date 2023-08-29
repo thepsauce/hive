535780bc073b272ee73403bf008d00fb4b699bbb
Vaxeral <vaxeral@gmail.com>
2023-08-28 21:04:27 -0400

    Move generation for each kind is done.  No need for hashmap, but we will need an array type.

--- src/move.c
+++ src/move.c
@@ -13,11 +13,15 @@ const unsigned char HIVE_SOUTH_BIT = HIVE_DIRECTION_BIT(HIVE_SOUTH);
 
 static unsigned char slide_bitset(unsigned char neigh)
 {
-    if ((neigh & (HIVE_NORTH_BIT | HIVE_SOUTH_EAST_BIT | HIVE_SOUTH_WEST_BIT)) ==
-                 (HIVE_NORTH_BIT | HIVE_SOUTH_EAST_BIT | HIVE_SOUTH_WEST_BIT))
+    if ((neigh & (HIVE_NORTH_BIT | HIVE_SOUTH_EAST_BIT | 
+    	          HIVE_SOUTH_WEST_BIT)) ==
+                 (HIVE_NORTH_BIT | HIVE_SOUTH_EAST_BIT | 
+                  HIVE_SOUTH_WEST_BIT))
         return 0;
-    if ((neigh & (HIVE_SOUTH_BIT | HIVE_NORTH_EAST_BIT | HIVE_NORTH_WEST_BIT)) ==
-                 (HIVE_SOUTH_BIT | HIVE_NORTH_EAST_BIT | HIVE_NORTH_WEST_BIT))
+    if ((neigh & (HIVE_SOUTH_BIT | HIVE_NORTH_EAST_BIT |
+    	          HIVE_NORTH_WEST_BIT)) ==
+                 (HIVE_SOUTH_BIT | HIVE_NORTH_EAST_BIT | 
+                  HIVE_NORTH_WEST_BIT))
         return 0;
 
     unsigned char avail = 0;
@@ -59,11 +63,12 @@ static unsigned char slide_bitset(unsigned char neigh)
     return avail & ~block;
 }
 
-static unsigned char neigh_bitset(struct hive *hive, struct vec3 pos, enum hive_side side)
+static unsigned char neigh_bitset(struct hive *hive,
+	struct vec3 *pos, enum hive_side side)
 {
     unsigned char neighBitset = 0;
     for(int i = 0; i < 6; ++i) {
-    	struct vec3 neighPos = vec_move(&pos, i);
+    	struct vec3 neighPos = vec_move(pos, i);
         piece_t neighPiece = hive_getexposedpiece(hive, &neighPos);
         if (neighPiece & side)
         	neighBitset |= HIVE_DIRECTION_BIT(i);
@@ -71,17 +76,17 @@ static unsigned char neigh_bitset(struct hive *hive, struct vec3 pos, enum hive_
     return neighBitset;
 }
 
-bool hive_can_place(struct hive *hive, struct vec3 pos, piece_t piece)
+bool hive_can_place(struct hive *hive, struct vec3 *pos, piece_t piece)
 {
 	return neigh_bitset(hive, pos, HIVE_GETSIDE(piece));
 }
 
 
-bool hive_one_hive(struct hive *hive, struct vec3 pos)
+bool hive_one_hive(struct hive *hive, struct vec3 *pos)
 {
 	int count = 0;
-	piece_t piece = hive_getexposedpiece(hive, &pos);
-	if (piece & HIVE_BELOW && pos.z > 0) {
+	piece_t piece = hive_getexposedpiece(hive, pos);
+	if (piece & HIVE_BELOW && pos->z > 0) {
 		return true;
 	}
 
@@ -95,7 +100,7 @@ bool hive_one_hive(struct hive *hive, struct vec3 pos)
 	piece_t adjPiece;
 
 	for (int i = 0; i < 6; i++) {
-		adjPos = vec_move(&pos, i);
+		adjPos = vec_move(pos, i);
 		adjPiece = hive_getabove(hive, &adjPos);
 		if (adjPiece) {
 			arrput(posQueue, adjPos);
@@ -104,7 +109,8 @@ bool hive_one_hive(struct hive *hive, struct vec3 pos)
 	}
 
 	while (arrlen(posQueue)) {
-		piece_t piece = hive->grid[adjPos.x + adjPos.y * GRID_COLUMNS];
+		struct vec3 pos = arrpop(posQueue);
+		piece_t piece = hive->grid[pos.x + pos.y * GRID_COLUMNS];
 		if (visited[HIVE_GETNPIECE(piece)] == false) {
 			visited[HIVE_GETNPIECE(piece)] == true;
 			count++;
@@ -112,7 +118,7 @@ bool hive_one_hive(struct hive *hive, struct vec3 pos)
 
 		for (int i = 0; i < 6; i++) {
 			adjPos = vec_move(&pos, i);
-			adjPiece = hive_getabove(hive, &adjPos);
+			adjPiece = hive->grid[adjPos.x + adjPos.y * GRID_COLUMNS];
 			if (adjPiece) {
 				arrput(posQueue, adjPos);
 				break;
@@ -135,22 +141,110 @@ static bool find_pos(const struct vec3 *posQueue, const struct vec3 *pos)
    	return false;
 }
 
-void hive_moves_for_ant(struct hive *hive, struct vec3 pos)
+void hive_moves_for_ant(struct hive *hive, struct vec3 *pos)
 {
     struct vec3 *posQueue = NULL;
-    arrput(posQueue, pos);
+    struct vec3 *movePos = NULL;
+    arrput(posQueue, *pos);
     while (arrlen(posQueue)) {
         struct vec3 currentPos = arrpop(posQueue);
-        unsigned char neigh = neigh_bitset(hive, currentPos, HIVE_WHITE & HIVE_BLACK);
+        unsigned char neigh = neigh_bitset(hive, &currentPos, 
+        	                       HIVE_WHITE & HIVE_BLACK);
         unsigned char slide = slide_bitset(neigh);
         for (int i = 0; i < 6; i++) {
             if(slide & HIVE_DIRECTION_BIT(i)) {
                 struct vec3 adjPos = vec_move(&currentPos, i);
                 if (find_pos(posQueue, &adjPos) == false) {
                 	arrput(posQueue, adjPos);
+                	arrput(movePos, adjPos);
                 }
             }
         }
     }
     arrfree(posQueue);
+    arrfree(movePos);
+}
+
+void hive_moves_for_spider(struct hive *hive, struct vec3 *pos)
+{
+	unsigned char neigh = neigh_bitset(hive, pos, HIVE_WHITE & HIVE_BLACK);
+    unsigned char slide = slide_bitset(neigh);
+    struct node {
+    	struct vec3 pos;
+    	struct vec3 previousPos;
+    	int depth;
+    } *nodeQueue = NULL;
+    struct vec3 *movePos = NULL;
+    arrput(nodeQueue, ((struct node){*pos, (struct vec3){0, 0, 0}, 0}));
+
+    while (arrlen(nodeQueue)) {
+    	struct node node = arrpop(nodeQueue);
+    	if (node.depth == 2) {
+    		arrput(movePos, node.pos);
+    	}
+    	unsigned char neigh = neigh_bitset(hive, 
+    		&node.pos, HIVE_WHITE & HIVE_BLACK);
+    	unsigned char slide = slide_bitset(neigh);
+    	for (int i = 0; i < 6; i++) {
+            if(slide & HIVE_DIRECTION_BIT(i)) {
+                struct vec3 adjPos = vec_move(&node.pos, i);
+                if (memcmp(&adjPos, &node.previousPos, sizeof node.previousPos) != 0) {
+                	arrput(nodeQueue, 
+                		((struct node){adjPos, node.pos, node.depth + 1}));
+                }
+            }
+    	}
+    }
+    arrfree(nodeQueue);
+    arrfree(movePos);
+}
+
+void hive_moves_for_grasshopper(struct hive *hive, struct vec3 *pos)
+{
+	struct vec3 *movePos = NULL;
+	for (int i = 0; i < 6; i++) {
+		struct vec3 adjPos = vec_move(pos, i);
+		piece_t adjPiece = hive->grid[adjPos.x + adjPos.y * GRID_COLUMNS];
+		if (adjPiece) {
+			while (adjPiece) {
+				adjPos = vec_move(&adjPos, i);
+				adjPiece = hive->grid[adjPos.x + adjPos.y * GRID_COLUMNS];
+			}
+			arrput(movePos, adjPos);
+		}
+	}
+	arrfree(movePos);
+}
+
+void hive_moves_for_queen(struct hive *hive, struct vec3 *pos)
+{
+    unsigned char neigh = neigh_bitset(hive, pos, HIVE_WHITE & HIVE_BLACK);
+    unsigned char slide = slide_bitset(neigh);
+    struct vec3 *movePos = NULL;
+    for(int i = 0; i < 6; ++i) {
+        if(slide & HIVE_DIRECTION_BIT(i)) {
+        	struct vec3 adjPos = vec_move(pos, i);
+        	arrput(movePos, adjPos);
+        }
+    }
+    arrfree(movePos);
+}
+
+void hive_moves_for_beetle(struct hive *hive, struct vec3 *pos)
+{
+	unsigned char neigh = neigh_bitset(hive, pos, HIVE_WHITE & HIVE_BLACK);
+    unsigned char slide;
+    struct vec3 *movePos = NULL;
+    piece_t piece = hive_getexposedpiece(hive, pos);
+    if (pos->z > 0)
+    	slide = 0xFF;
+    else
+    	slide = slide_bitset(neigh) & neigh;
+    for (int i = 0; i < 6; i++) {
+    	if (slide & HIVE_DIRECTION_BIT(i)) {
+    		struct vec3 adjPos = vec_move(pos, i);
+    		arrput(movePos, adjPos);
+    	}
+    }
+    arrfree(movePos);
 }
